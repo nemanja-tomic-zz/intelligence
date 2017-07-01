0821834b32442c01155e789a9f9bb38c1906962b
Nemanja Tomic <t.nemanja@hotmail.com>
2017-06-30 23:23:47 -0700

    Initial commit

---
+++ Intelligence/Intelligence/wwwroot/sketch.js
@@ -0,0 +1,23 @@
+﻿Tree tree;
+function setup() {
+    noCanvas();
+    tree = new Tree();
+    Node n = new Node(5);
+    tree.addNode(n);
+}
+
+function Tree() {
+    this.root = null;
+} 
+
+Tree.prototype.addNode = function (n) {
+    if (this.root = null) {
+        this.root = n;
+    }
+}
+
+function Node(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+}
