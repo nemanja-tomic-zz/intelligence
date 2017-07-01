bb4540461b857bd1689a150b08bb79f9ef608339
Nemanja Tomic <t.nemanja@hotmail.com>
2017-07-01 00:09:27 -0700

    Binary tree implementation.

--- Intelligence/Intelligence/wwwroot/sketch.js
+++ Intelligence/Intelligence/wwwroot/sketch.js
@@ -1,23 +1,10 @@
-﻿Tree tree;
+﻿var tree;
 function setup() {
-    noCanvas();
-    tree = new Tree();
-    Node n = new Node(5);
-    tree.addNode(n);
-}
-
-function Tree() {
-    this.root = null;
-} 
-
-Tree.prototype.addNode = function (n) {
-    if (this.root = null) {
-        this.root = n;
-    }
-}
-
-function Node(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+	noCanvas();
+	tree = new Tree();
+	for (var i = 0; i < 10; i++) {
+		tree.addValue(floor(random(0, 100)));
+	}
+	console.log(tree);
+	tree.traverse();
 }
